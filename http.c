13511399a3c465181eb909f76ad1fafb330ae4ef
Barbara Oliveira <barbara_oliveira@ufms.br>
2022-05-25 15:06:07 -0400

    verificações

--- http.c
+++ http.c
@@ -16,61 +16,70 @@ Professor: Rodrigo Kishi
 
 #define SIZE 1024
 
-void escreve_arquivo(char *buffer, int sockfd);
+void escreve_arquivo(char *buffer, int sock);
 
 int main(int argc, char *argv[]) {
 	struct addrinfo hints, *res; /*hints que especifica os requisitos para filtrar as estruturas de socket recuperadas, *res é onde a função irá alocar dinamicamente uma lista vinculada de estruturas addrinfo*/
-	int sockfd, i, j, cont;
-	char header[4096], *host, buffer[SIZE], url_aux[2083], *path;
-	host = argv[1]; 
+	int sock, i, j, cont, resp;
+	char request[SIZE], *url, buffer[SIZE], url_aux[2083], *path;
+	
+	if(argc < 2){
+        fprintf(stderr, "Modo de Uso: <./executavel + URL completa (com http://)>\n");
+        return 0;
+    }
+	
+	url = argv[1]; 
 
 	i = 7;
 	j = 0;
-	cont = strlen(host);
+	cont = strlen(url);
 
 	while(i<cont){
-		url_aux[j] = host[i];
+		url_aux[j] = url[i];
 		i++;
 		j++;
 	}
 
-	host = strtok(url_aux,"/");
-
+	url = strtok(url_aux,"/");
 	path = strtok(NULL, "\n");
-
+	
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	
-	getaddrinfo(host, "80", &hints, &res);
+	resp = getaddrinfo(url, "80", &hints, &res);
+	if(resp != 0){
+		fprintf(stderr, "Host não encontrado!\n");
+		exit(1);
+	}
 	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	connect(sockfd, res->ai_addr, res->ai_addrlen);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	connect(sock, res->ai_addr, res->ai_addrlen);
 
-	sprintf(header, "GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n", path, host);
+	sprintf(request, "GET /%s HTTP/1.0\r\nurl: %s\r\n\r\n", path, url);
 	
-	send(sockfd, header, strlen(header), 0);
+	send(sock, request, strlen(request), 0);
 	
 	memset(buffer, 0, SIZE);
-	recv(sockfd, buffer, SIZE, 0);
+	recv(sock, buffer, SIZE, 0);
 
 	if((strncmp("HTTP/1.0 200 OK", buffer, 15) != 0) && (strncmp("HTTP/1.1 200 OK", buffer, 15) != 0)){
 		printf("%s\n", buffer);
 	}else{
-		escreve_arquivo(buffer, sockfd);
+		escreve_arquivo(buffer, sock);
 	}
 	
 	return 0;
 }
 
-void escreve_arquivo(char *buffer, int sockfd){
+void escreve_arquivo(char *buffer, int sock){
 	FILE *arq;
 	
 	arq = fopen("http.html", "w");
 	if (arq == NULL){
 		fprintf(stderr, "Nao foi possivel abrir o arquivo\n");
 	}else{
-		while(recv(sockfd, buffer, SIZE, 0) > 0){
+		while(recv(sock, buffer, SIZE, 0) > 0){
 			fprintf(arq, "%s", buffer);
 			memset(buffer, 0, SIZE);
 		}
